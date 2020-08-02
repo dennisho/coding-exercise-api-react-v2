aeed9b9d9944552bcc34ff83db14889c824bbe00
Dennis Ho <dennisho@adventures.org>
2020-08-02 01:43:03 -0400

    upload csv files for both people and groups

--- src/uploadbutton.js
+++ src/uploadbutton.js
@@ -3,6 +3,40 @@ import { CSVReader } from 'react-papaparse';
 
 const buttonRef = React.createRef()
 
+async function fetchdata(url = '', data = {}, mtd = 'GET'){
+    // Default options are marked with *
+    const response = await fetch(url, {
+    method: mtd, // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+async function GETdataNoBody(url = ''){
+ 
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors', 
+    cache: 'no-cache',
+    credentials: 'same-origin', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  });
+  return response.json(); 
+}
+
 export default class CSVReader1 extends Component {
   handleOpenDialog = (e) => {
     // Note that the ref is set async, so it might be null at some point 
@@ -17,7 +51,136 @@ export default class CSVReader1 extends Component {
     console.log(data[1].data.length)
     console.log(data[1].data[1])
     console.log('---------------------------')
-    fetch('http://127.0.0.1:8000/api/groups',{
+    
+    if(data.length > 0) {
+      let csvType;
+      let fnurl;
+      let fndata;
+
+      if (data[0].data.length === 2){
+        //Groups
+        if( data[0].data.toString() === 'id,group_name'){
+          csvType = 'groups';
+          fnurl = 'http://127.0.0.1:8000/api/groups';
+          fndata = {group_name:''}
+          console.log(fndata);
+        }
+      }
+      else if (data[0].data.length === 5){
+        //People
+        csvType = 'people';
+        fnurl = 'http://127.0.0.1:8000/api/people';
+        fndata = {first_name:'',last_name:'',email_address:'',status:''};
+        console.log(fndata);
+      }
+      else {
+        console.log('Error: wrong number of columns.');
+        //throw new Error('Error: wrong number of columns.');
+        csvType = 'error';
+      }
+
+      if(csvType !== 'error') {
+        for(let i=1; i < data.length; i++ ){
+
+          GETdataNoBody(fnurl + '/' + data[i].data[0].toString())
+          .then(function(data2) {
+            console.log(data2);
+            //Has ID, update data
+            if(csvType === 'groups'){
+              fndata.group_name = data[i].data[1].toString();
+            } else if (csvType === 'people'){
+              fndata.first_name = data[i].data[1].toString();
+              fndata.last_name = data[i].data[2].toString();
+              fndata.email_address = data[i].data[3].toString();
+              fndata.status = data[i].data[4].toString();
+            }
+            fetchdata(fnurl + '/' + data[i].data[0].toString(),fndata,'PUT')
+            .then(function(data3) {
+              console.log(data3);
+              window.location.reload(false);
+            })
+            .catch(function(error) {
+              //console.error('Error:', error);
+            });
+          })
+          .catch(function(error) {
+            //console.error('Error:', error);
+            //Has not ID, insert data
+            if(csvType === 'groups'){
+              fndata.group_name = data[i].data[1].toString();
+            } else if (csvType === 'people'){
+              fndata.first_name = data[i].data[1].toString();
+              fndata.last_name = data[i].data[2].toString();
+              fndata.email_address = data[i].data[3].toString();
+              fndata.status = data[i].data[4].toString();
+            }
+            fetchdata(fnurl,fndata,'POST')
+            .then(function(data3) {
+              console.log(data3);
+              window.location.reload(false);
+            })
+            .catch(function(error) {
+              //console.error('Error:', error);
+            });
+          });
+        }
+      }
+
+      /*if(csvType !== 'error') {
+        for(let i=1; i < data.length; i++ ){
+
+          GETdataNoBody(fnurl + '/' + data[i].data[0].toString())
+          .then(data2 => {
+            console.log(data2);
+            //Has ID, update data
+            if(csvType === 'groups'){
+              fndata.group_name = data[i].data[1].toString();
+            } else if (csvType === 'people'){
+              fndata.first_name = data[i].data[1].toString();
+              fndata.last_name = data[i].data[2].toString();
+              fndata.email_address = data[i].data[3].toString();
+              fndata.status = data[i].data[4].toString();
+            }
+            fetchdata(fnurl + '/' + data[i].data[0].toString(),fndata,'PUT')
+            .then(data3 => {
+              console.log(data3);
+            })
+            .catch((error) => {
+              //console.error('Error:', error);
+            });
+          })
+          .catch((error) => {
+            //console.error('Error:', error);
+            //Has not ID, insert data
+            if(csvType === 'groups'){
+              fndata.group_name = data[i].data[1].toString();
+            } else if (csvType === 'people'){
+              fndata.first_name = data[i].data[1].toString();
+              fndata.last_name = data[i].data[2].toString();
+              fndata.email_address = data[i].data[3].toString();
+              fndata.status = data[i].data[4].toString();
+            }
+            fetchdata(fnurl,fndata,'POST')
+            .then(data3 => {
+              console.log(data3);
+            })
+            .catch((error) => {
+              //console.error('Error:', error);
+            });
+          });
+        }
+      }*/
+    }
+
+    /*fetchdata('http://127.0.0.1:8000/api/groups', { group_name: data[1].data[1] },'POST')
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });*/
+
+   /* fetch('http://127.0.0.1:8000/api/groups',{
       method: 'POST',
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -31,10 +194,9 @@ export default class CSVReader1 extends Component {
     })
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch(function(e){
-      
-    });
-
+    .catch((error) => {
+      console.error('Error:', error);
+    });*/
   }
 
   handleOnError = (err, file, inputElem, reason) => {
@@ -84,7 +246,7 @@ export default class CSVReader1 extends Component {
                 paddingRight: 0
               }}
             >
-              Browe file
+              Browse file
             </button>
             <div
               style={{
